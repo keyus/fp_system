bbec0b0041510f519e601bb033bd873d088ecf19
keyus <6325610@qq.com>
2018-02-24 17:10:16 +0800

    reset code

---
+++ src/view/login/login.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="login">
+        <div class="login_block">
+
+            <ul class="login_block__form">
+                <li>登录</li>
+                <li><input type="text" name="user" placeholder="请输入账户名"></li>
+                <li><input type="password" name="password"  placeholder="请输入密码"></li>
+                <li><button>登录</button></li>
+            </ul>
+
+            <ul class="login_block__list">
+                <li>扶贫对象系统</li>
+                <li>帮扶项目系统</li>
+                <li>帮扶单位</li>
+                <li>大数据分析</li>
+            </ul>
+
+        </div>
+
+        <div class="login_copyright">
+            © 2018-2025 大数据扶贫管理系统 。
+        </div>
+    </div>
+</template>
